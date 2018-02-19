0c7f30c6dd58fd1cb50364bd8217df5069426b53
kouts <koutsaftakis@gmail.com>
2018-02-19 14:44:38 +0200

    Implement separation between popup backdrop and popup wrapper.

--- dist/ractive-modal.js
+++ dist/ractive-modal.js
@@ -86,10 +86,10 @@ function getAnimationDuration(el){
     return (duration.indexOf('ms') !== -1) ? parseFloat(duration.replace('ms', '')) : parseFloat(duration.replace('s', '')) * 1000;
 }
 /* harmony default export */ __webpack_exports__["default"] = (Ractive.extend({
-    template: {v:4,t:[{t:4,f:[{t:7,e:"div",m:[{n:"role",f:"dialog",t:13},{n:"aria-label",f:[{t:2,r:"title"}],t:13},{n:"tabindex",f:"0",t:13},{n:"class",f:["popup-wrapper ",{t:2,r:"bg_class"}," ",{t:2,r:"base_anim_class"}," ",{t:2,r:"bg_anim_class"}," ",{t:2,r:"popup_id"}],t:13},{n:"style",f:["opacity: ",{t:2,r:"opacity"},"; display: ",{t:2,r:"display"},"; z-index: ",{t:2,r:"zindex"},"; cursor: ",{t:2,x:{r:["enableclose"],s:"_0?\"pointer\":\"default\""}}],t:13},{n:["click"],t:70,f:"close"},{n:["keydown"],t:70,f:"keydown"}],f:[{t:7,e:"div",m:[{n:"class",f:["popup ",{t:2,r:"base_anim_class"}," ",{t:2,r:"class"}," ",{t:2,r:"anim_class"}],t:13},{n:"style",f:[{t:2,r:"style"}],t:13},{n:["click"],t:70,f:{r:["@event"],s:"[_0.stopPropagation()]"}}],f:[{t:7,e:"div",m:[{n:"class",f:"popup-titlebar",t:13}],f:[{t:7,e:"h3",m:[{n:"class",f:"popup-title",t:13}],f:[{t:2,r:"title"}]}," ",{t:4,f:[{t:7,e:"button",m:[{n:"type",f:"button",t:13},{n:"class",f:"popup-btn-close",t:13},{n:["click"],t:70,f:"close"}],f:["×"]}],n:50,r:"enableclose"}]}," ",{t:7,e:"div",m:[{n:"class",f:"popup-content",t:13}],f:[{t:16}]}]}]}],n:50,r:"showpopup"}],e:{"_0?\"pointer\":\"default\"":function (_0){return(_0?"pointer":"default");},"[_0.stopPropagation()]":function (_0){return([_0.stopPropagation()]);}}},
-    css: ".popup-wrapper {position: fixed; text-align: center; top: 0; right: 0; bottom: 0; left: 0; z-index: 1050; overflow: auto; display: none; background-color: rgba(0, 0, 0, 0.5);} .popup {position: relative; display: inline-block; width:100%; text-align: left; color: #333333; background-color: #fff; top:30px; cursor: default; min-width: 110px; max-width: 500px; box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);} .popup-titlebar {padding:15px; color: #333; overflow: auto; border-bottom: 1px solid #e5e5e5;} .popup-title {margin-top:2px; margin-bottom: 0px; display: inline-block; font-size:1.25rem;} .popup-btn-close {padding: 0px 4px 0px 4px; cursor: pointer; background: 0 0; border: 0; font-weight:700; float: right; font-size: 1.5rem; line-height: 1; margin-right: -5px; color:#ccc;} .popup-btn-close:hover, .popup-btn-close:focus:hover{color:#6f6f6f; border-color: transparent; background-color: transparent;} .popup-btn-close:focus {color:#939393; border-color: transparent; background-color: transparent;} .popup-content {padding:15px 15px 15px 15px;} .popup-content .full-hr {width: auto; border: 0; border-top: 1px solid #e0e0e0; margin-top:15px; margin-bottom:15px; margin-left:-14px; margin-right:-14px;} .popup-fadeIn {-webkit-animation-name: popup-fadeIn; animation-name: popup-fadeIn;} @-webkit-keyframes popup-fadeIn {0% {opacity: 0} 100% {opacity: 1}} @keyframes popup-fadeIn {0% {opacity: 0} 100% {opacity: 1}} .popup-fadeOut {-webkit-animation-name: popup-fadeOut; animation-name: popup-fadeOut;} @-webkit-keyframes popup-fadeOut {0% {opacity: 1} 100% {opacity: 0}} @keyframes popup-fadeOut {0% {opacity: 1} 100% {opacity: 0}} .popup-fadeIn, .popup-fadeOut {-webkit-animation-duration: .25s; animation-duration: .25s; -webkit-animation-fill-mode: both; animation-fill-mode: both;}",
+    template: {v:4,t:[{t:4,f:[{t:7,e:"div",m:[{n:"class",f:["popup-backdrop ",{t:2,r:"bg_class"}," ",{t:2,r:"base_anim_class"}," ",{t:2,r:"bg_anim_class"}," backdrop-",{t:2,r:"popup_id"}],t:13},{n:"style",f:["z-index: ",{t:2,x:{r:["zindex"],s:"_0-1"}},";"],t:13}]}],n:50,r:"showbackdrop"},{t:4,f:[{t:7,e:"div",m:[{n:"class",f:["popup-wrapper ",{t:2,r:"base_anim_class"}," ",{t:2,r:"anim_class"}," ",{t:2,r:"popup_id"}],t:13},{n:"role",f:"dialog",t:13},{n:"aria-label",f:[{t:2,r:"title"}],t:13},{n:"tabindex",f:"0",t:13},{n:"style",f:["opacity: ",{t:2,r:"opacity"},"; display: ",{t:2,r:"display"},"; z-index: ",{t:2,r:"zindex"},"; cursor: ",{t:2,x:{r:["enableclose"],s:"_0?\"pointer\":\"default\""}}],t:13},{n:["click"],t:70,f:"close"},{n:["keydown"],t:70,f:"keydown"}],f:[{t:7,e:"div",m:[{n:"class",f:["popup ",{t:2,r:"class"}],t:13},{n:"style",f:[{t:2,r:"style"}],t:13},{n:["click"],t:70,f:{r:["@event"],s:"[_0.stopPropagation()]"}}],f:[{t:7,e:"div",m:[{n:"class",f:"popup-titlebar",t:13}],f:[{t:7,e:"h3",m:[{n:"class",f:"popup-title",t:13}],f:[{t:2,r:"title"}]}," ",{t:4,f:[{t:7,e:"button",m:[{n:"type",f:"button",t:13},{n:"class",f:"popup-btn-close",t:13},{n:["click"],t:70,f:"close"}],f:["×"]}],n:50,r:"enableclose"}]}," ",{t:7,e:"div",m:[{n:"class",f:"popup-content",t:13}],f:[{t:16}]}]}]}],n:50,r:"showpopup"}],e:{"_0-1":function (_0){return(_0-1);},"_0?\"pointer\":\"default\"":function (_0){return(_0?"pointer":"default");},"[_0.stopPropagation()]":function (_0){return([_0.stopPropagation()]);}}},
+    css: ".popup-backdrop {position: fixed; top: 0; right: 0; bottom: 0; left: 0; overflow: auto; background-color: rgba(0, 0, 0, 0.5);} .popup-wrapper {position: fixed; text-align: center; top: 0; right: 0; bottom: 0; left: 0; overflow: auto; display: none;} .popup {position: relative; display: inline-block; width:100%; text-align: left; color: #333333; background-color: #fff; top:30px; cursor: default; min-width: 110px; max-width: 500px; box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);} .popup-titlebar {padding:15px; color: #333; overflow: auto; border-bottom: 1px solid #e5e5e5;} .popup-title {margin-top:2px; margin-bottom: 0px; display: inline-block; font-size:1.25rem;} .popup-btn-close {padding: 0px 4px 0px 4px; cursor: pointer; background: 0 0; border: 0; font-weight:700; float: right; font-size: 1.5rem; line-height: 1; margin-right: -5px; color:#ccc;} .popup-btn-close:hover, .popup-btn-close:focus:hover{color:#6f6f6f; border-color: transparent; background-color: transparent;} .popup-btn-close:focus {color:#939393; border-color: transparent; background-color: transparent;} .popup-content {padding:15px 15px 15px 15px;} .popup-content .full-hr {width: auto; border: 0; border-top: 1px solid #e0e0e0; margin-top:15px; margin-bottom:15px; margin-left:-14px; margin-right:-14px;} .popup-fadeIn {-webkit-animation-name: popup-fadeIn; animation-name: popup-fadeIn;} @-webkit-keyframes popup-fadeIn {0% {opacity: 0} 100% {opacity: 1}} @keyframes popup-fadeIn {0% {opacity: 0} 100% {opacity: 1}} .popup-fadeOut {-webkit-animation-name: popup-fadeOut; animation-name: popup-fadeOut;} @-webkit-keyframes popup-fadeOut {0% {opacity: 1} 100% {opacity: 0}} @keyframes popup-fadeOut {0% {opacity: 1} 100% {opacity: 0}} .popup-fadeIn, .popup-fadeOut {-webkit-animation-duration: .25s; animation-duration: .25s; -webkit-animation-fill-mode: both; animation-fill-mode: both;}",
     data: function(){
-        return {showpopup: false, basedon: false, live: false, appendto: '', class: '', bg_class: '', base_anim_class: '', anim_class: '', bg_anim_class: '', bg_in_class: 'popup-fadeIn', bg_out_class: 'popup-fadeOut', in_class: 'popup-fadeIn', out_class: 'popup-fadeOut', anim_duration: '', style:'', title: '', popup_id: this._guid+'_popup', enableclose: true, base_zindex: 1050, opacity: 0, display: 'none', zindex: 0}
+        return {showpopup: false, showbackdrop: false, basedon: false, live: false, appendto: '', class: '', bg_class: '', base_anim_class: '', anim_class: '', bg_anim_class: '', bg_in_class: 'popup-fadeIn', bg_out_class: 'popup-fadeOut', in_class: 'popup-fadeIn', out_class: 'popup-fadeOut', anim_duration: '', style:'', title: '', popup_id: this._guid+'_popup', enableclose: true, base_zindex: 1051, opacity: 0, display: 'none', zindex: 0}
     },
     on: {
         close: function(ctx){
@@ -138,7 +138,7 @@ function getAnimationDuration(el){
                 this.elToFocus = document.activeElement;
                 this.fire('beforeOpen');
                 var lastZindex = this.getTopZindex();
-                var zindex = (lastZindex == 0) ? this.get('base_zindex') : lastZindex+1;
+                var zindex = (lastZindex == 0) ? this.get('base_zindex') : lastZindex+2;
                 if(!live){
                     this.set('showpopup', true);
                 }
@@ -190,23 +190,23 @@ function getAnimationDuration(el){
     animatePopup: function(dir){
         return new Promise(function(resolve, reject){
             var handler = function(e) {
-                this.find('.popup').removeEventListener('animationend', handler);
+                this.find('.popup-wrapper').removeEventListener('animationend', handler);
                 this.set({'anim_class': '', 'bg_anim_class': ''});
                 Ractive._modal_animating = undefined;
                 Ractive._modal_delay = 0;
                 if(dir == 'out'){
-                    this.set('opacity', 0);
+                    this.set({showbackdrop: false, opacity: 0});
                 }
                 resolve();
             }.bind(this);
-            this.find('.popup').addEventListener('animationend', handler);
+            this.find('.popup-wrapper').addEventListener('animationend', handler);
             if(typeof Ractive._modal_animating === 'object'){
-                Ractive._modal_delay = Ractive._modal_delay + getAnimationDuration(Ractive._modal_animating.find('.popup')) + 100;
+                Ractive._modal_delay = Ractive._modal_delay + getAnimationDuration(Ractive._modal_animating.find('.popup-wrapper')) + 100;
             }
             Ractive._modal_animating = this;
             setTimeout(function(){
                 if(dir == 'in'){
-                    this.set('opacity', 1);
+                    this.set({showbackdrop: true, opacity: 1});
                 }
                 this.set({'anim_class': this.get(dir+'_class'), 'bg_anim_class': this.get('bg_'+dir+'_class')});
             }.bind(this), Ractive._modal_delay);
