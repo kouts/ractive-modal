e559d7c1f843ff33ee54fb7dcf16b75e537b86f8
E6440\Kouts <koutsaftakis@gmail.com>
2018-12-11 10:02:42 +0200

    Updated build to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,40 @@
-var path = require('path');
-var webpack = require('webpack');
-
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common');
-
-// Lazymode so we don't have to require Ractive in every component / endpoint / etc
-/*
-var providerPlugin = new webpack.ProvidePlugin({
-	Ractive: 'ractive'
-});
-*/
-
-// Minify javascript
-var minifyPlugin = new webpack.optimize.UglifyJsPlugin({
-	include: /\.min\.js$/,
-	compress: {
-		warnings: false
-	}
-});
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-	module: {
-	  loaders: [
-	    {
-	      test: /\.ractive\.html$/,
-	      loaders: 'ractive-bin-loader'
-	    }
-	  ]
-	},
-	plugins: [
-		// providerPlugin,
-		minifyPlugin
-	],
-	externals: {
-	  ractive: 'Ractive'
-	},
-	target: 'web',
-	entry: {
-		'ractive-modal': './src/ractive-modal.ractive.html',
-		'ractive-modal.min': './src/ractive-modal.ractive.html'
-	},
-  	output: {
-	    filename: '[name].js',
-	    path: path.resolve(__dirname, 'dist'),
-	    library: "RactiveModal",
-	    libraryExport: "default",
-		libraryTarget: "umd"
-  	}
-};
+	mode: 'development',
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    plugins: [
+    ],    
+    entry: {
+        'ractive-modal': './ractive-modal.ractive.html'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: 'RactiveModal',
+        libraryTarget: 'umd'
+    },
+    externals: {
+    	ractive: {
+    		commonjs: 'Ractive',
+    		commonjs2: 'Ractive',
+    		amd: 'Ractive',
+    		root: 'Ractive'
+    	}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ractive\.html$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'ractive-bin-loader'
+                    }                    
+                ]
+            }
+        ]
+    }
+}
---
+++ webpack.config.old.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common');
+
+// Lazymode so we don't have to require Ractive in every component / endpoint / etc
+/*
+var providerPlugin = new webpack.ProvidePlugin({
+	Ractive: 'ractive'
+});
+*/
+
+// Minify javascript
+var minifyPlugin = new webpack.optimize.UglifyJsPlugin({
+	include: /\.min\.js$/,
+	compress: {
+		warnings: false
+	}
+});
+
+module.exports = {
+	module: {
+	  loaders: [
+	    {
+	      test: /\.ractive\.html$/,
+	      loaders: 'ractive-bin-loader'
+	    }
+	  ]
+	},
+	plugins: [
+		// providerPlugin,
+		minifyPlugin
+	],
+	externals: {
+	  ractive: 'Ractive'
+	},
+	target: 'web',
+	entry: {
+		'ractive-modal': './src/ractive-modal.ractive.html',
+		'ractive-modal.min': './src/ractive-modal.ractive.html'
+	},
+  	output: {
+	    filename: '[name].js',
+	    path: path.resolve(__dirname, 'dist'),
+	    library: "RactiveModal",
+	    libraryExport: "default",
+		libraryTarget: "umd"
+  	}
+};
