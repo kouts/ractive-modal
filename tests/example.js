4f6ffd6beeda3b38b4f56f2478fcfa894e42d990
E6440\Kouts <koutsaftakis@gmail.com>
2019-11-25 10:09:31 +0200

    Starting tests

---
+++ tests/example.js
@@ -0,0 +1,44 @@
+import { getByLabelText, getByText, getByTestId, queryByTestId, wait, getQueriesForElement } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
+import Ractive from 'ractive';
+
+Ractive.components.counter = Ractive.extend({
+	template: `
+		<div>
+			<button on-click="@this.add('count')">
+				{{count}}
+			</button>
+		</div>
+	`,
+	data: function(){
+		return {
+			count: 0
+		}
+	}
+});
+
+function render(Component) {
+	const div = document.createElement('div');
+	div.id = 'app';
+	const r = new Ractive({
+		target: div,
+		template: `
+			<div>
+				<counter />
+			</div>
+		`
+	});
+	return div;
+}
+
+// tests:
+test('counter increments', () => {
+	const div = render();
+	const {getByText} = getQueriesForElement(div);
+	  const { getByText } = getQueriesForElement(div)
+	  const counter = getByText('0');
+	  fireEvent.click(counter);
+	  expect(counter).toHaveTextContent('1');
+	  fireEvent.click(counter)
+	  expect(counter).toHaveTextContent('2');
+});
